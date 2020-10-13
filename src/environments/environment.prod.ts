df007801312551f40584314925046f59dc048492
oNionUxx <omerlubko@gmail.com>
2020-10-13 18:57:22 +0300

    ready for deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,3 +2,8 @@ export const environment = {
   production: true,
   apiURL: 'https://manof-application.herokuapp.com',
 };
+
+export const globals = {
+  googleMapsKey: 'AIzaSyDMhL-OZ-tuGTmgTeH_IKV8bj1M3d7rlvs',
+  activityTab: 1,
+};
